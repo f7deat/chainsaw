f557a99245fc4e79aa8d2c78b1485465937e1d8e
f7deat <tan.dct9x@gmail.com>
2023-04-05 17:18:52 +0700

    Updates

---
+++ _next/static/n_j_bJlgWPNqDuL69TlpO/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/710-ef96c3cfdfd54059.js","static/css/bff950a31bfd5d70.css","static/chunks/pages/index-6aca32b5fbd3a083.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
