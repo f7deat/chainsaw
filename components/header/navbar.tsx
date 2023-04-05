7a305694026ba0ee6d3ea751def6b26492d83287
f7deat <tan.dct9x@gmail.com>
2023-04-05 16:21:52 +0700

    try to deploy

--- components/header/navbar.tsx
+++ components/header/navbar.tsx
@@ -5,7 +5,9 @@ const Navbar: React.FC = () => {
         <nav className="bg-orange-500">
             <div className="flex container mx-auto">
                 <button className="h-16 flex">
-                    <img src="https://xcdn-cf.vuihoc.vn/upload/5c209fe6176b0/2022/02/18/7d72_logo-vuihoc-bitu-normal.png" className="h-12" />
+                    <picture>
+                        <img src="https://xcdn-cf.vuihoc.vn/upload/5c209fe6176b0/2022/02/18/7d72_logo-vuihoc-bitu-normal.png" alt="logo" className="h-12" />
+                    </picture>
                 </button>
                 <div className="h-16 gap-2 text-white font-bold items-center flex">
                     <BookIcon />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+// import { Quicksand } from 'next/font/google'
+
+// const quickSand = Quicksand({ 
+//   subsets: ['latin'],
+//   weight: ['400', '500', '700']
+//  })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  )
 }
