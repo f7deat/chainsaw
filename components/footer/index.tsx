a8dffd2244e62b54ceaeab35344e4e6161db610a
f7deat <tan.dct9x@gmail.com>
2023-04-06 09:57:36 +0700

    remove git action and package lock

---
+++ components/footer/index.tsx
@@ -0,0 +1,12 @@
+const Footer: React.FC = () => {
+    return (
+        <footer>
+            <div style={{
+                minHeight: 450,
+                backgroundImage: 'url(https://static.xx.vuihoc.vn/theme/vuihoc/imgs/footer_img_svg.png)'
+            }} className="bg-no-repeat bg-bottom"></div>
+        </footer>
+    )
+}
+
+export default Footer
--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
   images: {
     loader: 'akamai',
     path: '',
-  },
+  }
 }
 
 module.exports = nextConfig
